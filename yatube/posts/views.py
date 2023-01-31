c73a3d4d8b5bc95d781c6c979f15dc38ec54222d
Дмитрий Капустин <kda99@mail.ru>
2023-02-01 01:12:16 +0300

    ok

---
+++ yatube/posts/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+
+from .models import Post, Group
+from .forms import PostForm
+
+User = get_user_model()
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядеть так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10)
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = Post.objects.filter(group=group).order_by('-pub_date')
+    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    post_list = Post.objects.all().filter(author__exact=user)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': user,
+        'page_obj': page_obj,
+        'num_of_pub': len(post_list),
+
+    }
+
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    post = get_object_or_404(Post, id=post_id)
+    num_of_pub = Post.objects.filter(author__exact=post.author).count()
+    author = post.author
+    username = author.username
+    context = {
+        'post': post,
+        'num_of_pub': num_of_pub,
+        'username': username,
+        'author': author
+    }
+
+    return render(request, 'posts/post_detail.html', context)
+
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    if form.is_valid():
+        form.instance.author = request.user
+        form.save()
+        return redirect('posts:profile', request.user.username)
+    return render(request, 'posts/create_post.html', context)
+
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None)
+    current_user = request.user
+    is_edit = True if current_user == post.author else False
+    context = {
+        'is_edit': is_edit,
+        'form': form,
+    }
+    if not is_edit:
+        return redirect('posts:post_detail', post_id)
+    if form.is_valid():
+        form.instance.author = request.user
+        form.save()
+        return redirect('posts:post_detail', post_id)
+    return render(request, 'posts/update_post.html', context)
