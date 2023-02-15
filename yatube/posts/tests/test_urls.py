884a5828d010bd1a59aeb2526c5b6324d9fb938c
Дмитрий Капустин <kda99@mail.ru>
2023-02-15 07:53:58 +0300

    Спринт 5/16 → Тема 2/4: Тестирование Django → Урок 3/8 - осталось расписать тесты

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,12 +1,63 @@
-# posts/tests/test_urls.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
+from ..models import Group, Post
 
-class StaticURLTests(TestCase):
-    def test_homepage(self):
-        # Создаем экземпляр клиента
-        guest_client = Client()
-        # Делаем запрос к главной странице и проверяем статус
-        response = guest_client.get('/')
-        # Утверждаем, что для прохождения теста код должен быть равен 200
-        self.assertEqual(response.status_code, 200)
+User = get_user_model()
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.author = User.objects.create_user(username='author')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            text='Тестовый пост',
+            author=cls.author,
+            group=cls.group,
+            id=1
+        )
+
+    def setUp(self):
+        # Создаем неавторизованный клиент
+        self.guest_client = Client()
+        # Создаем пользователя
+        self.user = User.objects.create_user(username='HasNoName')
+        # Создаем второй клиент
+        self.authorized_client = Client()
+        self.authorized_client_author = Client()
+        # Авторизуем пользователя
+        self.authorized_client.force_login(self.user)
+        self.authorized_client_author.force_login(self.author)
+
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        # Шаблоны по адресам
+        templates_url_names = {
+            '/': 'posts/index.html',
+            '/group/test-slug/': 'posts/group_list.html',
+            '/profile/author/': 'posts/profile.html',
+            f'/posts/{self.post.id}/edit/': 'posts/new_post.html',
+            '/posts/1/': 'posts/post_detail.html',
+            '/create/': 'posts/new_post.html',
+        }
+
+        # for address, template in templates_url_names.items():
+        #     with self.subTest(address=address):
+        #         response = self.guest_client.get(address)
+        #         self.assertTemplateUsed(response, template)
+        #
+        # for address, template in templates_url_names.items():
+        #     with self.subTest(address=address):
+        #         response = self.authorized_client.get(address)
+        #         self.assertTemplateUsed(response, template)
+
+        for address, template in templates_url_names.items():
+            with self.subTest(address=address):
+                response = self.authorized_client_author.get(address)
+                self.assertTemplateUsed(response, template)
